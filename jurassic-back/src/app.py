a380e4f76b3c7104354fe064269a0a5d8f1a4fb8
pabloin <pablo.eze.inchausti@gmail.com>
2022-09-11 20:12:27 -0300

    ID-#12 Flask Server para el Backend de Jurassic Site

---
+++ jurassic-back/src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask,jsonify,request
+
+from src.db.db_manager import load_dinos, save_dinos
+
+
+app = Flask(__name__)
+
+@app.get('/api/jurassic/hello')
+def get_dinos():
+    return 'Hello!'
+
+
+@app.get('/api/jurassic/dinos')
+def get_hello():
+    lista_dinos = load_dinos()
+    return jsonify([restr.serialize() for restr in lista_dinos])
+
+
+@app.route('/api/jurassic/dinos', methods = ['POST'])
+def do_post():
+    data = request.get_json()
+
+    save_dinos(data)
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
