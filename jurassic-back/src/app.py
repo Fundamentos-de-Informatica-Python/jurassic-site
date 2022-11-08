142462f8309d00ae6a303feb37e0b9959141ed71
pablo.inchausti <pablo.inchausti@bbva.com>
2022-11-08 12:04:50 -0300

    ID-#3 Postman Collection added

--- jurassic-back/src/app.py
+++ jurassic-back/src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify,request
 from flask_cors import CORS
 
-from src.db.db_manager import load_dinos, save_dinos
+from db.db_manager import load_dinos, save_dinos
 
 app = Flask(__name__)
 CORS(app)
